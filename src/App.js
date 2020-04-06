0c6305661b825475b46f955dc87c727df0f0f6b7
Benjamin Fry <benjaminpfry@gmail.com>
2020-04-06 02:35:50 -0400

    Added some cleanup and comments before trying to make events week-specific

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import EventSpace from './EventSpace';
 
 function App() {
   const sayHello = () => console.log("Hello There");
-  let events = [{"id":1, "name":"General Physics II for Biology Majors", "time":"03 April 2020 09:00:00 EDT", "ends": "03 April 2020 09:50:00 EDT"}, {"id":2, "name":"Organic Chemistry Lecture", "time":"03 April 2020 10:00:00 EDT", "ends": "03 April 2020 10:50:00 EDT"}];
+  let events = [{"id":1, "name":"General Physics II for Biology Majors", "time":"12 April 2020 09:00:00 EDT", "ends": "13 April 2020 09:50:00 EDT"}, {"id":2, "name":"Organic Chemistry Lecture", "time":"03 April 2020 10:00:00 EDT", "ends": "03 April 2020 10:50:00 EDT"}];
   return (
     <div className="App">
       <div className="content">
--- src/EventSpace.js
+++ src/EventSpace.js
@@ -3,21 +3,10 @@ import CalendarEvent from './CalendarEvent';
 import './App.css';
 
 let days = [1, 2, 3, 4, 5, 6, 0]
-let test = [[], [], [], [], [], [], []]    
+let test = [[], [], [], [], [], [], []]
 
 
-let EventGrid = ({eventlist, day, test}) => {
-
-
-    //  eventlist.forEach(item => {
-    //      let parsed = new Date(Date.parse(item.time));
-    //      let eventday = parsed.getDay();
-    //      if (eventday === day){
-    //          test[day].push(<CalendarEvent day={day} key={item.id} time={item.time} ends={item.ends} title={item.name}/>)
-    //      } else {
-
-    //     }
-    // })
+let EventGrid = ({ eventlist, day, test }) => {
 
     for (let i = 0; i < eventlist.length; i++) {
         let parsed = new Date(Date.parse(eventlist[i].time));
@@ -29,79 +18,76 @@ let EventGrid = ({eventlist, day, test}) => {
         let totaltop = hourtop + minutetop;
 
         let parsed2 = new Date(Date.parse(eventlist[i].ends));
-        let msbetween = Math.abs(parsed2-parsed);
-        let minbetweenheight = (msbetween/60000)*0.104167;
+        let msbetween = Math.abs(parsed2 - parsed);
+        let minbetweenheight = (msbetween / 60000) * 0.104167;
 
-        if (eventday === day){
+        if (eventday === day) {
 
             if (totaltop + minbetweenheight > 150) {
                 let overflowheight = (totaltop + minbetweenheight) - 150;
-                test[day].push(<CalendarEvent key={eventlist[i].id} totaltop={totaltop} totalheight={minbetweenheight-overflowheight} title={eventlist[i].name}/>)
+                test[day].push(<CalendarEvent key={eventlist[i].id} totaltop={totaltop} totalheight={minbetweenheight - overflowheight} title={eventlist[i].name} />)
                 switch (day) {
                     case 6:
-                        test[0].push(<CalendarEvent key={eventlist[i].id} totaltop={0} totalheight={overflowheight} title={eventlist[i].name}/>)
+
+                        //this doesnt work because monday has already been drawn at this point. Howver, realistically this doesnt matter, we just want it to go to the next day so need to conver the list to use Date objects rather than simple indices
+
+                        test[0].push(<CalendarEvent key={eventlist[i].id} totaltop={0} totalheight={overflowheight} title={eventlist[i].name} />)
                         break
                     default:
-                        test[day+1].push(<CalendarEvent key={eventlist[i].id} totaltop={0} totalheight={overflowheight} title={eventlist[i].name}/>)
+                        test[day + 1].push(<CalendarEvent key={eventlist[i].id} totaltop={0} totalheight={overflowheight} title={eventlist[i].name} />)
                         break
                 }
-    
+
             } else {
-                test[day].push(<CalendarEvent key={eventlist[i].id} totaltop={totaltop} totalheight={minbetweenheight} title={eventlist[i].name}/>)
-    
+                test[day].push(<CalendarEvent key={eventlist[i].id} totaltop={totaltop} totalheight={minbetweenheight} title={eventlist[i].name} />)
+
             }
 
         } else {
             continue;
         }
-
-
-        
     }
-
-
     return (
-    <div className="eventgrid">
-        {test[day].map(item => (
-            item
-    ))}
+        <div className="eventgrid">
+            {test[day].map(item => (
+                item
+            ))}
 
-    </div>
-   )
+        </div>
+    )
 
 
 }
 
 console.log(test)
 
-let EventSpace = ({eventlist}) => {
-    console.log(eventlist)
+let EventSpace = ({ eventlist }) => {
     eventlist.forEach(item => {
         console.log(item);
     });
     return (
         <div className="eventspace">
-            {}        
+            {}
             <div className="spacer">
             </div>
             {days.map(day => (
-                
-                <div 
-                key = {day}
-                className="eventunit" 
-                id = {day}>
 
-                <EventGrid
-                test = {test}
-                eventlist = {eventlist}
-                key = {day}
-                day = {day}
-                />
+                <div
+                    key={day}
+                    className="eventunit"
+                    id={day}>
+
+                    <EventGrid
+                        test={test}
+                        eventlist={eventlist}
+                        key={day}
+                        day={day}
+                    />
 
                     <div className="displayspacer">
                     </div>
                 </div>
-            ))}     
+            ))}
         </div>
     )
 }
