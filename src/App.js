30bf6cbaa0b17afe31a0a4e1fd42e63621a6d062
Benjamin Fry <benjaminpfry@gmail.com>
2020-04-07 12:12:30 -0400

    Moved the week list generation up the stack but now need to wrap sunday-monday overlap into a new list

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Calendar from './Calendar';
 import CalendarHead from './CalendarHead';
 import EventSpace from './EventSpace';
+import CalendarEvent from './CalendarEvent'
 
 //date needs to be equal a string exactly 1 week +/- the current Date().
 let updateDays = (newweekdate = null) => {
   let curr;
-  if (newweekdate===null) {
+  if (newweekdate === null) {
     curr = new Date();
   } else {
     curr = new Date(newweekdate);
@@ -37,46 +38,102 @@ let updateDays = (newweekdate = null) => {
     }
   }
   console.log(temp)
-  
-  return(temp)
+
+  return (temp)
 
 }
-
 
 function App() {
 
   let week = [];
-  let [inc, setinc]=useState(0)
-  if (inc===0){
+  let weekofevents = [[], [], [], [], [], [], []];
+  let [inc, setinc] = useState(0)
+
+  if (inc === 0) {
     week = updateDays();
   } else {
-    week = [];
     let tempDate = new Date();
-    let weekinms = 7*24*60*60*1000;
-    week = updateDays(tempDate.getTime()+weekinms)
+    let daynum;
+    switch (tempDate.getDay()) {
+      case 0:
+        daynum = 6;
+        break;
+      default:
+        daynum = tempDate.getDay() - 1;
+        break;
+    }
+    let dayforadd = week[daynum];
+    week = [];
+    let weekinms = 7 * 24 * 60 * 60 * 1000;
+    week = updateDays(dayforadd.getTime() + weekinms)
   }
 
   let increment = () => {
-    setinc(inc+1)
+    setinc(inc + 1)
   }
   //want to use this syntax to change what week it is. Hook? The week is an empty array by default but we can set it to something with the update days function. 
   //const sayHello = () => console.log("Hello There");
-
-
   let events = [{ "id": 1, "name": "General Physics II for Biology Majors", "time": "13 April 2020 09:00:00 EDT", "ends": "14 April 2020 09:50:00 EDT" }, { "id": 2, "name": "Organic Chemistry Lecture", "time": "07 April 2020 10:00:00 EDT", "ends": "07 April 2020 10:50:00 EDT" }];
+
+  //test
+  for (let i = 0; i < events.length; i++) {
+    let parsed = new Date(Date.parse(events[i].time));
+
+    let eventday = parsed.getDate();
+
+    let hourtop = parsed.getHours() * 6.25;
+    let minutetop = parsed.getMinutes() * 0.104167;
+    let totaltop = hourtop + minutetop;
+
+    let parsed2 = new Date(Date.parse(events[i].ends));
+    let msbetween = Math.abs(parsed2 - parsed);
+    let minbetweenheight = (msbetween / 60000) * 0.104167;
+
+
+    for (let z = 0; z < week.length; z++) {
+      let day = week[z]
+      console.log(day)
+
+      if (eventday === day.getDate()) {
+        if (totaltop + minbetweenheight > 150) {
+          let overflowheight = (totaltop + minbetweenheight) - 150;
+          weekofevents[day.getDay()].push(<CalendarEvent key={events[i].id} totaltop={totaltop} totalheight={minbetweenheight - overflowheight} title={events[i].name} />)
+          switch (day) {
+            case 6:
+              //this doesnt work because monday has already been drawn at this point. Howver, realistically this doesnt matter, we just want it to go to the next day so need to conver the list to use Date objects rather than simple indices
+              weekofevents[0].push(<CalendarEvent key={events[i].id} totaltop={0} totalheight={overflowheight} title={events[i].name} />)
+              break
+            default:
+              weekofevents[day.getDay() + 1].push(<CalendarEvent key={events[i].id} totaltop={0} totalheight={overflowheight} title={events[i].name} />)
+              break
+          }
+
+        } else {
+          weekofevents[day.getDay()].push(<CalendarEvent key={events[i].id} totaltop={totaltop} totalheight={minbetweenheight} title={events[i].name} />)
+
+        }
+
+      } else {
+        continue;
+      }
+    }
+  }
+  //end test
+
+  console.log(weekofevents)
 
   return (
     <div className="App">
       <div className="content">
         <div className="upper">
-          <CalendarHead weekdays={week}/>
+          <CalendarHead weekdays={week} />
         </div>
         <div className="lower">
           <div className="scrollcontainercontainer">
             <div className="scrollcontainer">
               <div className="scroller">
                 <Calendar />
-                <EventSpace eventlist={events} week={week}/>
+                <EventSpace events={events} weekofevents={weekofevents} />
               </div>
             </div>
           </div>
--- src/EventSpace.js
+++ src/EventSpace.js
@@ -7,61 +7,18 @@ let days = [1, 2, 3, 4, 5, 6, 0]
 
 let EventGrid = ({ eventlist, day, weekofevents }) => {
 
-    for (let i = 0; i < eventlist.length; i++) {
-        let parsed = new Date(Date.parse(eventlist[i].time));
-
-        let eventday = parsed.getDay();
-
-        let hourtop = parsed.getHours() * 6.25;
-        let minutetop = parsed.getMinutes() * 0.104167;
-        let totaltop = hourtop + minutetop;
-
-        let parsed2 = new Date(Date.parse(eventlist[i].ends));
-        let msbetween = Math.abs(parsed2 - parsed);
-        let minbetweenheight = (msbetween / 60000) * 0.104167;
-
-        if (eventday === day) {
-
-            if (totaltop + minbetweenheight > 150) {
-                let overflowheight = (totaltop + minbetweenheight) - 150;
-                weekofevents[day].push(<CalendarEvent key={eventlist[i].id} totaltop={totaltop} totalheight={minbetweenheight - overflowheight} title={eventlist[i].name} />)
-                switch (day) {
-                    case 6:
-
-                        //this doesnt work because monday has already been drawn at this point. Howver, realistically this doesnt matter, we just want it to go to the next day so need to conver the list to use Date objects rather than simple indices
-
-                        weekofevents[0].push(<CalendarEvent key={eventlist[i].id} totaltop={0} totalheight={overflowheight} title={eventlist[i].name} />)
-                        break
-                    default:
-                        weekofevents[day + 1].push(<CalendarEvent key={eventlist[i].id} totaltop={0} totalheight={overflowheight} title={eventlist[i].name} />)
-                        break
-                }
-
-            } else {
-                weekofevents[day].push(<CalendarEvent key={eventlist[i].id} totaltop={totaltop} totalheight={minbetweenheight} title={eventlist[i].name} />)
-
-            }
-
-        } else {
-            continue;
-        }
-    }
     return (
         <div className="eventgrid">
             {weekofevents[day].map(item => (
                 item
             ))}
-
         </div>
     )
-
-
 }
 
-let EventSpace = ({ eventlist, week, overflow }) => {
-    let weekofevents = [[], [], [], [], [], [], []]
+let EventSpace = ({ events, weekofevents, overflow }) => {
 
-    eventlist.forEach(item => {
+    events.forEach(item => {
         console.log(item);
     });
     return (
@@ -78,7 +35,7 @@ let EventSpace = ({ eventlist, week, overflow }) => {
 
                     <EventGrid
                         weekofevents={weekofevents}
-                        eventlist={eventlist}
+                        events={events}
                         key={day}
                         day={day}
                     />
