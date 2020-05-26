9216fb9ee29005a64552e117846d67b488c277b7
Ben Fry <benfyo@hotmail.com>
2020-05-26 16:21:10 -0400

    finished PUT request functionality. Added new popup window to handle event edits on clicking title. Need to edit API to handle PUT

--- src/App.js
+++ src/App.js
@@ -5,9 +5,12 @@ import CalendarHead from './components/CalendarHead';
 import EventSpace from './components/EventSpace';
 import CalendarEvent from './components/CalendarEvent';
 import NewEventForm from './components/NewEventForm';
+import EditPopup from './components/EditPopup'
 import {ParseResponse} from './api'
 
+let processedevents = [];
 
+//This function is used to generate an array of the datetimes for the current week being looked at and will take in a date and generate the week associated with that date or null which returns the week based on todays date. 
 let updateDays = (newweekdate = null) => {
   //This function takes in a day or nothing (to get current week) and returns an array of the days of the current week which is later stored into the 'week' array
   let curr;
@@ -44,22 +47,45 @@ let updateDays = (newweekdate = null) => {
 
 }
 
-let processedevents = [];
-
 function App() {
+  //This hook controls how many rerenders as well as how many API requests occur. Fetchagain is passed into api functions and only when true will a new request be made. Prevents fetching upon every render of App().
   let [fetchagain, setfetchagain] = useState(false)
-  processedevents = ParseResponse(fetchagain)
-  
 
+  //Sents the above initialized processed events array equal to a processed response from the API functions. The fetchagain parameter ensures it only runs when we want it to.
+  processedevents = ParseResponse(fetchagain)
+
+  //initialize the week array to be empty and then fill it below
   let week = [];
   let weekofevents = [[], [], [], [], [], [], []];
 
-  let [popup, setPopup] = useState(false)
+  //Hook used to display the NewEventPopup when popup is true.
+  let [popup, setPopup] = useState(false);
   const showNewEventPopUp = () => {
     setPopup(!popup)
   };
 
+
+
+  let [editevent, setEditEvent] = useState(false);
+  let [eventforedit, setEventForEdit] = useState(null);
+
+  const showEditEventPopup = (id) => {
+      console.log(id);
+      setEventForEdit(id);
+      setEditEvent(true);
+  }
+
+  const hideEditEventPopup = () => {
+    setEditEvent(false);
+    setEventForEdit(null);
+  }
+
+
+
+  //Hook for incrementing the counter to advance pages in the calendar. 
   let [inc, setinc] = useState(0)
+
+  //Here we fill the week array with the update days function which is defined above. 
   week = updateDays();
 
   let tempDate = new Date();
@@ -77,16 +103,18 @@ function App() {
   //daynum just gives the day of the week
   let dayforadd = week[daynum];
   week = [];
+  //this value is the number of milliseconds in a week and it is multiplied by the increment value below. An inc value of zero returns current week. Greater than 0 advances by that number of weeks and less than 0 goes back that number of weeks.
   let weekinms = 7 * 24 * 60 * 60 * 1000;
   week = updateDays(dayforadd.getTime() + (weekinms * inc))
 
-  //!want to use this syntax to change what week it is. Hook? The week is an empty array by default but we can set it to something with the update days function. 
+  //these functions are called on click of the FA arrows that allow for the week to be advanced
   let increment = () => {
     setinc(inc + 1)
   }
   let deincrement = () => {
     setinc(inc - 1)
   }
+  //function resets the calendar to the current week. 
   let reset = () => {
     setinc(0)
   }
@@ -102,13 +130,13 @@ function App() {
       let day = week[z]
       if ((parsedtemp.getDate() === day.getDate()) && (parsedtemp.getMonth() === day.getMonth()) && (parsedtemp.getFullYear() === day.getFullYear())) {
         //checks if the event its looking at is in the week, on the month, of the year and if it is, checks the overflow array for the same event
-          weekofevents[day.getDay()].push(<CalendarEvent key={processedevents[i].key} totaltop={processedevents[i].totaltop} totalheight={processedevents[i].totalheight} title={processedevents[i].title} repeator={processedevents[i].repeator} number={processedevents[i].id} deletefun={setfetchagain}/>)
+          weekofevents[day.getDay()].push(<CalendarEvent key={processedevents[i].key} totaltop={processedevents[i].totaltop} totalheight={processedevents[i].totalheight} title={processedevents[i].title} repeator={processedevents[i].repeator} number={processedevents[i].id} deletefun={setfetchagain} showEditEventPopup = {showEditEventPopup}/>)
         } 
       }
     }
   }
 
-
+  //determines the month of the first day in the week event and allows this to be displayed at the top of the calendar. 
   let month;
   switch (week[0].getMonth()) {
     case 0:
@@ -148,7 +176,7 @@ function App() {
       month = "December"
       break;
   }
-  
+  //This performs the formatting for the month name at the top of the page. 
   let monthyear = `${month} ${week[0].getFullYear()}`;
   
   return (
@@ -177,6 +205,16 @@ function App() {
             </div>            
           <NewEventForm setfetchagain={setfetchagain} setPopup={setPopup}/>
           </div>
+
+          <div className="neweventpopup" style = {{display : editevent ? "inline-block" : "none"}}>
+            <div className="topbar">
+            <h3>Edit an Event</h3>
+            <p className="closepopup" onClick={hideEditEventPopup}>x</p>
+            </div>
+
+            <EditPopup eventforedit = {eventforedit} eventlist = {processedevents} hidepopup = {hideEditEventPopup} refresh = {setfetchagain}/>
+          </div>
+
           <div className="neweventcircle" onClick={showNewEventPopUp}>
             <i className="fas fa-plus"></i>
           </div>
---
+++ src/components/EditPopup.js
@@ -0,0 +1,93 @@
+import React, {useState, useEffect} from 'react';
+import '../App.css';
+import DatePicker from "react-datepicker"
+import "react-datepicker/dist/react-datepicker.css"
+import {PutRequest} from "../api";
+
+let EditPopup = ({eventforedit, eventlist, hidepopup, refresh}) => {
+
+    const [newName, setNewName] = useState("");
+    const [startDate, setStartDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(new Date());
+
+    useEffect(() => {
+        var test = eventlist ? eventlist.find(obj => {return obj.id === eventforedit}) : "";
+        var titleforedit = test ? test.title : "";
+        var startforedit = test ? new Date(parseInt(test.ostarted)) : new Date();
+        var endforedit = test ? new Date (parseInt(test.oended)) : new Date();
+
+        setNewName(titleforedit);
+        setStartDate(startforedit);
+        setEndDate(endforedit);
+    }, [eventforedit, eventlist])
+
+
+    let handleClick = (e) => {
+        e.preventDefault();
+        if (newName === "") {
+          alert("Hey! Name can't be blank!");
+        } else if ((endDate.getTime() - startDate.getTime()) < 0){
+          alert("Hey! End date can't be before start date");
+        } else {
+          let start = startDate.getTime();
+          let end = endDate.getTime();
+
+            PutRequest(eventforedit, newName, start, end, refresh);
+
+            setNewName('');
+            setStartDate(new Date());
+            setEndDate(new Date());
+            hidepopup();
+        }
+    }
+
+    return(
+        <form>
+
+        <div className="inputlayout">
+        <label htmlFor="name">Name</label>
+        <div className="NameContainer">
+        <input type="text" id="name" value={newName} onChange={e => setNewName(e.target.value)}/>
+        </div>
+        </div>
+
+
+        <div className="inputlayout">
+        <label htmlFor="date">Start Date</label>
+        <div className="testwrap">
+          <DatePicker
+            selected={startDate}
+            onChange={date => setStartDate(date)}
+            showTimeSelect
+            timeFormat="h:mm aa"
+            timeIntervals={15}
+            timeCaption="Start Time"
+            dateFormat="MMMM d, yyyy h:mm aa"
+          />
+        </div>
+        </div>
+
+        <div className="inputlayout">
+        <label htmlFor="date">End Date</label>
+          <div className="testwrap">
+          <DatePicker
+            selected={endDate}
+            minDate={startDate}
+            onChange={date => setEndDate(date)}
+            showTimeSelect
+            timeFormat="h:mm aa"
+            timeIntervals={15}
+            timeCaption="End Time"
+            dateFormat="MMMM d, yyyy h:mm aa"
+          />
+          </div>
+        </div>
+
+        <button className="finalsubmit" type="submit" onClick={handleClick}>Submit</button>
+      </form>
+
+    )
+
+}
+
+export default EditPopup;
