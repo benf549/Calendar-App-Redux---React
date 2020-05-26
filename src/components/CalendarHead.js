b49d7558aca90540e004fae09b74d9849ff87329
Ben Fry <benfyo@hotmail.com>
2020-05-25 23:57:55 -0400

    added highlight in dayheader of current day

--- src/components/CalendarHead.js
+++ src/components/CalendarHead.js
@@ -5,7 +5,8 @@ import "../App.css"
 
 let Day = (param) => {
     return (
-        <div className="dayheader">
+        <div className="dayheader" style={ param.td ? {backgroundColor:  "var(--red-accent)", borderRadius : "5px"} : null}>
+          {console.log(param)}
             <h5>{param.day}</h5>
             <h1>{param.date}</h1>
         </div>
@@ -13,63 +14,54 @@ let Day = (param) => {
 }
 
 let CalendarHead = ({weekdays}) => {
-    let days = []
+    let days = [];
+    let today = new Date();
 
     for (let i = 0; i < weekdays.length; i++) {
+
+      var daypush = {
+        "date": weekdays[i].getDate(),
+        "isToday": ((weekdays[i].getDate() === today.getDate()) && (weekdays[i].getMonth() === today.getMonth()) && (weekdays[i].getFullYear() === today.getFullYear())) ? true : false
+        }
+
+        var dow;
         switch (i) {
           case 0:
-            days.push({
-              "day": "Mon",
-              "date": weekdays[0].getDate()
-            })
+            dow = "Mon";
             break;
           case 1:
-            days.push({
-              "day": "Tue",
-              "date": weekdays[1].getDate()
-            })
+            dow = "Tue";
             break;
           case 2:
-            days.push({
-              "day": "Wed",
-              "date": weekdays[2].getDate()
-            })
+            dow = "Wed";
             break;
           case 3:
-            days.push({
-              "day": "Thu",
-              "date": weekdays[3].getDate()
-            })
+            dow = "Thu";
             break;
           case 4:
-            days.push({
-              "day": "Fri",
-              "date": weekdays[4].getDate()
-            })
+            dow = "Fri";
             break;
           case 5:
-            days.push({
-              "day": "Sat",
-              "date": weekdays[5].getDate()
-            })
+            dow = "Sat";
             break;
           default:
-            days.push({
-              "day": "Sun",
-              "date": weekdays[6].getDate()
-            })
+            dow = "Sun";
             break;
         }
+        daypush.day = dow;
+        days.push(daypush)
       }
 
     return(
         <div className="dayheadercontainer">
             <div className="spacer"></div>
             {days.map(item => (
+
                 <Day 
                 key={item.date}
                 day={item.day}
                 date={item.date}
+                td={item.isToday}
                 />
             ))}
         </div>
