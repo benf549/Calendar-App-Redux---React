658e6bbc0ceeeeace1de26a159fb744d5db4ba15
Benf549 <benjaminpfry@gmail.com>
2020-06-09 17:21:06 -0400

    Changed the screen height to 100% so that it doesnt scroll any more. Will add back if needed.

--- src/components/useHover.js
+++ src/components/useHover.js
@@ -33,7 +33,7 @@ function useHover() {
         };
       }
     },
-    [ref.current] // Recall only if ref changes
+    // [ref.current] // Recall only if ref changes
   );
 
   return [ref, value];
