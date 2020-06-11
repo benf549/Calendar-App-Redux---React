b0e926254e79f0e76d888611447f8129680b4ad8
Benf549 <benjaminpfry@gmail.com>
2020-06-11 09:48:05 -0400

    fixed some hover functionality. Need to figure out why the mouseLeave event isnt working as expected.

--- src/components/CalendarEvent.js
+++ src/components/CalendarEvent.js
@@ -1,10 +1,84 @@
-import React from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import "../App.css";
-import useHover from './useHover.js'
-import {DeleteRequest} from "../api"
+import {DeleteRequest} from "../api";
+
+function getWindowDimensions() {
+    const { innerWidth: width, innerHeight: height } = window;
+    return {
+      width,
+      height
+    };
+}
+
+//  function useTest() {
+//      const [height, setHeight] = useState(0);
+//      const ref2 = useRef(null);
+  
+//      const handleMouseOver = (prop) => setHeight(prop);
+//      const handleMouseOut = () => setHeight(0);
+
+//      useEffect(
+//        () => {
+//          const node = ref2.current;
+//          if (node) {
+//            node.addEventListener('mouseover', handleMouseOver(node.clientHeight));
+//            node.addEventListener('mouseout', handleMouseOut);
+  
+//            return () => {
+//              node.removeEventListener('mouseover', handleMouseOver);
+//              node.removeEventListener('mouseout', handleMouseOut);
+//            };
+//          }
+//        },
+//        //[ref2.current] // Recall only if ref changes
+//      );
+  
+//      return [ref2, height];
+//    }
+
+  export function useWindowDimensions() {
+    const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+  
+    useEffect(() => {
+      function handleResize() {
+        setWindowDimensions(getWindowDimensions());
+      }
+  
+      window.addEventListener('resize', handleResize);
+      return () => window.removeEventListener('resize', handleResize);
+    }, []);
+  
+    return windowDimensions;
+}
+
 
 let CalendarEvent = ({totaltop, totalheight, title, repeator, number, deletefun, showEditEventPopup}) => {
-    const [hoverRef, isHovered] = useHover(totalheight);
+    const {height} = useWindowDimensions();
+    const [height2, setHeight2] = useState(0);
+    const ref2 = useRef(null);
+
+    const handleenter= () => {
+      setHeight2(ref2.current.clientHeight)
+      console.log(height2)
+    }
+
+    const handleexit = () => {
+      setHeight2(0)
+      console.log(height2)
+    }
+
+ 
+    const Testt = {
+          "--hovered-element-height" : `${height2 > totalheight*0.01*height ? height2 : totalheight *0.01*height}px`
+    }
+
+     if (height2) {
+         console.log(height2)
+         console.log(`Window Height:${height}px \nElement Height in px: ${totalheight*0.01*height} \nTextelementHeight=${height2}`)
+         document.documentElement.style.setProperty("--hovered-element-height", Testt["--hovered-element-height"]);
+     }
+                    
+
     
     if (repeator === 0) {
         //The second to nth block that repeats for a repeater.
@@ -24,8 +98,8 @@ let CalendarEvent = ({totaltop, totalheight, title, repeator, number, deletefun,
     } else {
         return (
         // Corresponds to events that dont span multiple days or weeks.
-        <div  className="Event" ref={hoverRef} style={{zIndex:"3", top:totaltop+'vh', height: isHovered ? "auto" : totalheight+"vh", borderBottomLeftRadius:'3px', borderBottomRightRadius:'3px'}}>
-            <p className = "eventtitle" onClick = {() => {showEditEventPopup(number)}}>{title}</p>
+        <div  className="Event" style={{zIndex:"3", top:totaltop+'vh', height:totalheight+"vh", borderBottomLeftRadius:'3px', borderBottomRightRadius:'3px'}}>
+            <p className = "eventtitle" ref={ref2} onMouseEnter={handleenter} onMouseLeave={handleexit} onClick = {() => {showEditEventPopup(number)}}>{title}</p>
             <p className="DEB t3" onClick={() => {DeleteRequest(number, deletefun)}}>X</p>
         </div>
         )
