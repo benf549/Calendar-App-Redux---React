6b4962479ad252c1826e92494ab7649720916b78
Benf549 <benjaminpfry@gmail.com>
2020-06-19 23:25:50 -0400

    Fixed minute formatting for numbers less than 10. I think next need to add a neweventform for todos

--- src/components/InWeekTDView.js
+++ src/components/InWeekTDView.js
@@ -2,13 +2,12 @@ import React from 'react';
 import {ParseFetchData} from '../api/todo';
 
 const ToDoEvent = ({name, time, priority, iscomplete}) => {
-
     return (
         <div className="todoitem">
             <div className="markoff" style={priority === 5 ? {backgroundColor:"rgba(255,0,0,0.3)", boxShadow: "inset 0 0 1vw var(--red-accent)"} : {backgroundColor:"rgba(0,0,0,0.3)", boxShadow: "inset 0 0 1vw black"}}></div>
             <div className="belowtodoitem"> 
                 <h4 style={{color : iscomplete ? "grey" : "black"}}>{name}</h4>
-                <p id="todotime">{`${time.getHours() > 12 ? (time.getHours - 12) : (time.getHours() === 0) ? 12 : time.getHours()}:${time.getMinutes()}${time.getHours() > 11 ? "pm" : "am"}`}</p>
+                <p id="todotime">{`${time.getHours() > 12 ? (time.getHours() - 12) : (time.getHours() === 0) ? 12 : time.getHours()}:${time.getMinutes() < 10 ? `0${time.getMinutes()}` : time.getMinutes() }${time.getHours() > 11 ? "pm" : "am"}`}</p>
             </div>            
         </div>
     )
