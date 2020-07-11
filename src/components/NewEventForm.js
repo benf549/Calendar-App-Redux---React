8e4c6512a59542bd8e0eed589164518e2ed304a8
Benf549 <benjaminpfry@gmail.com>
2020-07-11 18:51:56 -0400

    Added a day of the week picker for new events that pushes the daycodes selected into an array. Needs to be integrated with the actualy put function still

--- src/components/NewEventForm.js
+++ src/components/NewEventForm.js
@@ -1,96 +1,155 @@
-import React, {useState, useEffect} from "react"
-import "../App.css"
-import DatePicker from "react-datepicker"
-import "react-datepicker/dist/react-datepicker.css"
-import {PostData} from "../api"
+import React, { useState, useEffect } from "react";
+import "../App.css";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { PostData } from "../api";
 
+let weekcodes = ["M", "T", "W", "R", "F", "S", "D"];
 
-function NewEventForm({setfetchagain, setPopup, showPopup}) {
-    const [newName, setNewName] = useState('');
-    const [startDate, setStartDate] = useState(new Date());
-    const [endDate, setEndDate] = useState(new Date());
+function NewEventForm({ setfetchagain, setPopup, showPopup }) {
+	const [newName, setNewName] = useState("");
+	const [startDate, setStartDate] = useState(new Date());
+	const [endDate, setEndDate] = useState(new Date());
+	const [week_freq, setWeek_Freq] = useState(1);
+	const [selecteddays, setSelectedDays] = useState("");
 
-    let handleClick = (e) => {
-      e.preventDefault();
-      if (newName === "") {
-        alert("Hey! Name can't be blank!")
-      } else if ((endDate.getTime() - startDate.getTime()) <= 0){
-        alert("Hey! End date can't be before start date")
-      } else {
-        let start = startDate.getTime()
-        let end = endDate.getTime()
-        PostData({newName, start, end, setfetchagain})
-        setNewName('')
-        setStartDate(new Date())
-        setEndDate(new Date())
-        setPopup(false)
-      }
-    }
+	let handleClick = (e) => {
+		e.preventDefault();
+		if (newName === "") {
+			alert("Hey! Name can't be blank!");
+		} else if (endDate.getTime() - startDate.getTime() <= 0) {
+			alert("Hey! End date can't be before start date");
+		} else {
+			let start = startDate.getTime();
+			let end = endDate.getTime();
+			PostData({ newName, start, end, setfetchagain });
+			setNewName("");
+			setStartDate(new Date());
+			setEndDate(new Date());
+			setPopup(false);
+		}
+	};
 
-  // Allows you to close the new event form by pressing esc key
-    useEffect(() => {
-      const handleEsc = (event) => {
-          if (showPopup && event.keyCode === 27) {
-            setPopup(false)
-            console.log("closed")
-          }
-      };
-      window.addEventListener('keydown', handleEsc);
-  
-      return () => {
-        window.removeEventListener('keydown', handleEsc);
-      };
-    }, [showPopup, setPopup]);
+	let handleselect = (day) => {
+		if (handleEmphasize(day)) {
+			setSelectedDays(selecteddays.filter((item) => item !== day));
+		} else {
+			setSelectedDays((selecteddays) => [...selecteddays, day]);
+		}
+	};
 
-    useEffect(() => {
-      setEndDate(startDate)
-    }, [startDate])
+	let handleEmphasize = (item) => {
+		console.log(selecteddays);
+		let response = false;
+		for (let i = 0; i < selecteddays.length; i++) {
+			if (item === selecteddays[i]) {
+				response = true;
+			}
+		}
+		return response;
+	};
+	// useEffect(() => {
+	// 	console.log(selecteddays);
 
-     return (
-      <form>
+	// 	let response = handleEmphasize();
+	// }, [selecteddays]);
 
-        <div className="inputlayout">
-        <label htmlFor="name">Name</label>
-        <div className="NameContainer">
-        <input type="text" id="name" value={newName} onChange={e => setNewName(e.target.value)}/>
-        </div>
-        </div>
+	// Allows you to close the new event form by pressing esc key
+	useEffect(() => {
+		const handleEsc = (event) => {
+			if (showPopup && event.keyCode === 27) {
+				setPopup(false);
+				console.log("closed");
+			}
+		};
+		window.addEventListener("keydown", handleEsc);
 
+		return () => {
+			window.removeEventListener("keydown", handleEsc);
+		};
+	}, [showPopup, setPopup]);
 
-        <div className="inputlayout">
-        <label htmlFor="date">Start Date</label>
-        <div className="testwrap">
-          <DatePicker
-            selected={startDate}
-            onChange={date => setStartDate(date)}
-            showTimeSelect
-            timeFormat="h:mm aa"
-            timeIntervals={15}
-            timeCaption="Start Time"
-            dateFormat="MMMM d, yyyy h:mm aa"
-          />
-        </div>
-        </div>
+	useEffect(() => {
+		setEndDate(startDate);
+	}, [startDate]);
 
-        <div className="inputlayout">
-        <label htmlFor="date">End Date</label>
-          <div className="testwrap">
-          <DatePicker
-            selected={endDate}
-            minDate={startDate}
-            onChange={date => setEndDate(date)}
-            showTimeSelect
-            timeFormat="h:mm aa"
-            timeIntervals={15}
-            timeCaption="End Time"
-            dateFormat="MMMM d, yyyy h:mm aa"
-          />
-          </div>
-        </div>
+	return (
+		<form>
+			<div className="inputlayout">
+				<label htmlFor="name">Name</label>
+				<div className="NameContainer">
+					<input
+						type="text"
+						id="name"
+						value={newName}
+						onChange={(e) => setNewName(e.target.value)}
+					/>
+				</div>
+			</div>
 
-        <button className="finalsubmit" type="submit" onClick={handleClick}>Submit</button>
-      </form>
-     )
+			<div className="inputlayout">
+				<label htmlFor="date">Start Date</label>
+				<div className="testwrap">
+					<DatePicker
+						selected={startDate}
+						onChange={(date) => setStartDate(date)}
+						showTimeSelect
+						timeFormat="h:mm aa"
+						timeIntervals={15}
+						timeCaption="Start Time"
+						dateFormat="MMMM d, yyyy h:mm aa"
+					/>
+				</div>
+			</div>
+
+			<div className="inputlayout">
+				<label htmlFor="date">End Date</label>
+				<div className="testwrap">
+					<DatePicker
+						selected={endDate}
+						minDate={startDate}
+						onChange={(date) => setEndDate(date)}
+						showTimeSelect
+						timeFormat="h:mm aa"
+						timeIntervals={15}
+						timeCaption="End Time"
+						dateFormat="MMMM d, yyyy h:mm aa"
+					/>
+				</div>
+			</div>
+			<button className="finalsubmit" type="submit" onClick={handleClick}>
+				Submit
+			</button>
+			<div className="bottombarwrap">
+				{weekcodes.map((item) => {
+					return (
+						<div
+							className={`repeatcircle ${
+								handleEmphasize(item) ? "selected" : null
+							}`}
+							key={item}
+							onClick={() => handleselect(item)}
+						>
+							<p className="repeatday" key={item}>
+								{item === "R" ? "T" : item === "D" ? "S" : item}
+							</p>
+						</div>
+					);
+				})}
+				<input
+					type="text"
+					id="rep_frequency"
+					value={week_freq}
+					onChange={(e) =>
+						setWeek_Freq(e.target.value >= 0 ? e.target.value : 1)
+					}
+				></input>
+				<div className="frequencylabelborder">
+					<p className="frequencylabel">{week_freq <= 1 ? "Week" : "Weeks"}</p>
+				</div>
+			</div>
+		</form>
+	);
 }
 
-export default NewEventForm
+export default NewEventForm;
